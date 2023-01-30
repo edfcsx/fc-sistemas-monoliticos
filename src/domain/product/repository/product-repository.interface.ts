7ff273761b9cfb595e857623eb996b0025f4a8ff
Silva, Eduardo Felipe <edfcsx@gmail.com>
2023-01-30 12:58:01 -0300

    initial files

--- src/domain/product/repository/product-repository.interface.ts
+++
@@ -1,4 +0,0 @@
-import Product from '../../product/entity/product'
-import RepositoryInterface from '../../@shared/repository/repository-interface'
-
-export default interface ProductRepositoryInterface extends RepositoryInterface<Product> {}
